a241149a2e15c7733f0cdd94cc577b9fbbe5ac8b
Sergio Vinícius <sj.vinicius@hotmail.com>
2024-11-20 22:15:40 -0300

    dia 3

--- server.js
+++ server.js
@@ -1,56 +1,9 @@
 import express from "express";
-
-const posts = [
-    {
-        id: 1,
-        descricao: "Uma foto qualquer",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 2,
-        descricao: "Paisagem deslumbrante",
-        imagem: "https://source.unsplash.com/random/300x200"
-    },
-    {
-        id: 3,
-        descricao: "Cachorro fofo",
-        imagem: "https://picsum.photos/id/237/300/200"
-    },
-    {
-        id: 4,
-        descricao: "Comida deliciosa",
-        imagem: "https://loremflickr.com/300/200/food"
-    },
-    {
-        id: 5,
-        descricao: "Cidade vibrante",
-        imagem: "https://unsplash.com/photos/random"
-    },
-    {
-        id: 6,
-        descricao: "Natureza exuberante",
-        imagem: "https://source.unsplash.com/random/?nature"
-    }
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
-app.use(express.json())
+routes(app);
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-});
-
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarPostPorId(id){
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    });
-}
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorId(req.params.id);
-    res.status(200).json(posts[index]);
 });
